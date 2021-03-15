53929396a41e05f3816ca14b2b38c6484be7dcab
lena <alena.litouka@gmail.com>
2021-03-15 00:33:31 -0400

    finished with some help

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,35 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.TreeMap;
 
-public class Bins{
+public class Bins {
 
-    public TreeMap<Integer, Integer> countTotals=new TreeMap<Integer, Integer>();
+    public TreeMap<Integer, Integer> countTotals = new TreeMap<Integer, Integer>();
     int numOfDice;
     int numOfKeys;
 
     public Bins(Dice dice){
-        this.numOfDice= dice.getNumberOfDice();
+        this.numOfDice=dice.getNumberOfDice();
         this.numOfKeys=dice.getNumberOfDice()*6;
+
     }
+
     public void createStorageBin(){
-        for(int i=2;i<=numOfKeys;i++){
-            countTotals.put(i,0);
+        //Integer numberOfKeysNeeded = (dice.getNumberOfDice() * 6);
+
+        for(int i = 2; i <= getNumOfKeys(); i++) {
+            countTotals.put(i, 0);
         }
     }
 
     public void storeCountInBin(Dice dice, Integer numberOfRolls){
         createStorageBin();
-        for(int i=0;i<getNumOfDice();i++){
-            int key=dice.diceRoll();
-            int value=countTotals.get(key);
-            value+=1;
-            countTotals.put(key,value);
+        for(int i = 0; i < numberOfRolls; i++){
+            int key = dice.diceRoll();
+            int value = countTotals.get(key);
+            value += 1;
+            countTotals.put(key, value);
         }
     }
-    public int getNumOfDice(){
+
+    public int getNumOfDice() {
         return numOfDice;
     }
-    public int getNumOfKeys(){
+
+    public int getNumOfKeys() {
         return numOfKeys;
     }
-
 }
--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,18 +1,25 @@
+
+import java.util.Random;
+
 public class Dice {
-public int numberOfDice;
 
-public Dice(int numberOfDice){
-    this.numberOfDice=numberOfDice;
-}
-public Integer diceRoll(){
-    int sum=0;
-    for(int i=0;i<numberOfDice;i++){
-        sum+=(int)(Math.random()*6)+1;
+    public int numberOfDice;
+
+    public Dice(int numOfDice) {
+        this.numberOfDice = numOfDice;
     }
-    return sum;
-}
-public int getNumberOfDice(){
-    return numberOfDice;
-}
 
+    public Integer diceRoll() {
+
+        int totalSumOfDice = 0;
+        for (int i = 0; i < numberOfDice; i++) {
+            totalSumOfDice += (int) Math.floor(Math.random() * 6) + 1;
+
+        }
+        return totalSumOfDice;
+    }
+
+    public int getNumberOfDice() {
+        return numberOfDice;
+    }
 }
