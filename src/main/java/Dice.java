68cceac5a8eedba02ff8cfe0c929eee3abb1eed3
lena <alena.litouka@gmail.com>
2021-03-14 21:10:19 -0400

    dice class

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,14 @@
 public class Dice {
+public int value;
 
-
+public Dice(){
+    this.value=(int)(Math.random()*6+1);//toss
+}
+public Integer tossAndSum(){
+    Dice dice1=new Dice();
+    Dice dice2=new Dice();
+    Integer sum;
+    sum=dice1.value+ dice2.value;
+    return sum;
+}
 }
